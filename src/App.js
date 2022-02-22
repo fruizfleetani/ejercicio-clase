f72086a1ef433f52c8120135bae07291323642a9
fruizfleetani <93193248+fruizfleetani@users.noreply.github.com>
2022-02-22 09:26:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends React.Component {
   render(){
   return (
     <div>
-    <Header titulo={this.state.titulo}/>
-    <Body tituloModificado={this.modificarTitulo}/>
+    <Cabecera titulo={this.state.titulo}/>
+    <Cuerpo tituloModificado={this.modificarTitulo}/>
     </div>
   );
   }
